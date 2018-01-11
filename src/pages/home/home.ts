38d5a23faf65e954c977573a01f471da837afb20
Teun Verhaert <teun.verhaert@student.ap.be>
2018-01-11 13:47:45 +0100

    pages toegevoegd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {SignupPage} from "../signup/signup";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  signup = SignupPage;
 
   constructor(public navCtrl: NavController) {
 
